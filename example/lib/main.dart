f2279e27010942571fb09def70aaad8c9b48c84a
ja2375 <webmaster@javi-h.com>
2019-02-08 12:54:46 +0100

    Bump to 1.2.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
       title: 'Test event',
       description: 'example',
       location: 'Flutter app',
-      startDate: DateTime(2019, 1, 31, 16, 49),
-      endDate: DateTime(2019, 2, 1, 17, 01),
+      startDate: DateTime(2019, 2, 8, 16, 49),
+      endDate: DateTime(2019, 2, 9, 17, 01),
       allDay: true,
     );
 
