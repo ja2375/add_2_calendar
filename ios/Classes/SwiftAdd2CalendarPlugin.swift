281d6acb8b88d1facf6d25c91ebc6e4482154513
abdel ali <35116901+a6delali@users.noreply.github.com>
2019-11-12 18:09:21 +0100

    Manage calendar events with EventKitUI (#24)
    * Manage calendar events with EventKitUI
    
    * update cocoapods configuration to swift 4.2

--- ios/Classes/SwiftAdd2CalendarPlugin.swift
+++ ios/Classes/SwiftAdd2CalendarPlugin.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import EventKit
+import EventKitUI
 import Foundation
 
 extension Date {
@@ -10,52 +11,115 @@ extension Date {
 }
 
 public class SwiftAdd2CalendarPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter.javih.com/add_2_calendar", binaryMessenger: registrar.messenger())
-    let instance = SwiftAdd2CalendarPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+      let channel = FlutterMethodChannel(name: "flutter.javih.com/add_2_calendar", binaryMessenger: registrar.messenger())
+      let instance = SwiftAdd2CalendarPlugin()
+      registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method == "add2Cal" {
-        let args = call.arguments as! [String:Any]
-        let title = args["title"] as! String
-        let desc = args["desc"] as! String
-        let location = args["location"] as! String
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+      if call.method == "add2Cal" {
+          let args = call.arguments as! [String:Any]
+          let title = args["title"] as! String
+          let desc = args["desc"] as! String
+          let location = args["location"] as! String
+          
+          addEventToCalendar(title: title,
+                             description: desc,
+                             location: location,
+                             startDate: Date(milliseconds: (args["startDate"] as! Double)),
+                             endDate: Date(milliseconds: (args["endDate"] as! Double)),
+                             allDay: args["allDay"] as! Bool,
+                             completion: { (success) -> Void in
+              if success {
+                  result(true)
+              } else {
+                  result(false)
+              }
+          })
+      }
+    }
+
+    private func addEventToCalendar(title: String!, description: String, location: String, startDate: Date, endDate: Date, allDay: Bool, completion: ((_ success: Bool) -> Void)? = nil) {
+        let eventStore = EKEventStore()
         
-        addEventToCalendar(title: title, description: desc, location: location, startDate: Date(milliseconds: (args["startDate"] as! Double)), endDate: Date(milliseconds: (args["endDate"] as! Double)), allDay: args["allDay"] as! Bool, completion: { (success) -> Void in
-            if success {
-                result(true)
+        eventStore.requestAccess(to: .event, completion: { [weak self] (granted, error) in
+            if (granted) && (error == nil) {
+                let event = EKEvent(eventStore: eventStore)
+                event.title = title
+                event.startDate = startDate
+                event.endDate = endDate
+                event.location = location
+                event.notes = description
+                event.isAllDay = allDay
+                self?.presentCalendarModalToAddEvent(event, eventStore: eventStore, completion: completion)
             } else {
-                result(false)
+                completion?(false)
             }
         })
     }
-  }
 
-    private func addEventToCalendar(title: String!, description: String, location: String, startDate: Date, endDate: Date, allDay: Bool, completion: ((_ success: Bool) -> Void)? = nil) {
-          let eventStore = EKEventStore()
-
-          eventStore.requestAccess(to: .event, completion: { (granted, error) in
-              if (granted) && (error == nil) {
-                  let event = EKEvent(eventStore: eventStore)
-                  event.title = title
-                  event.startDate = startDate
-                  event.endDate = endDate
-                  event.location = location
-                  event.notes = description
-                  event.isAllDay = allDay
-                  event.calendar = eventStore.defaultCalendarForNewEvents
-                  do {
-                      try eventStore.save(event, span: .thisEvent)
-                  } catch let e as NSError? {
-                      completion?(false)
-                      return
-                  }
-                  completion?(true)
-              } else {
-                  completion?(false)
-              }
-          })
-  }
+    private func getAuthorizationStatus() -> EKAuthorizationStatus {
+        return EKEventStore.authorizationStatus(for: EKEntityType.event)
+    }
+    
+    // Show event kit ui to add event to calendar
+    
+    func presentCalendarModalToAddEvent(_ event: EKEvent, eventStore: EKEventStore, completion: ((_ success: Bool) -> Void)? = nil) {
+        let authStatus = getAuthorizationStatus()
+        switch authStatus {
+        case .authorized:
+            OperationQueue.main.addOperation {
+                self.presentEventCalendarDetailModal(event: event, eventStore: eventStore)
+            }
+            completion?(true)
+        case .notDetermined:
+            //Auth is not determined
+            //We should request access to the calendar
+            eventStore.requestAccess(to: .event, completion: { [weak self] (granted, error) in
+                if granted {
+                    OperationQueue.main.addOperation {
+                        self?.presentEventCalendarDetailModal(event: event, eventStore: eventStore)
+                    }
+                    completion?(true)
+                } else {
+                    // Auth denied
+                    completion?(false)
+                }
+            })
+        case .denied, .restricted:
+            // Auth denied or restricted
+            completion?(false)
+        }
+    }
+    
+    // Present edit event calendar modal
+    
+    func presentEventCalendarDetailModal(event: EKEvent, eventStore: EKEventStore) {
+        let eventModalVC = EKEventEditViewController()
+        eventModalVC.event = event
+        eventModalVC.eventStore = eventStore
+        eventModalVC.editViewDelegate = self
+        
+        if #available(iOS 13, *) {
+            eventModalVC.modalPresentationStyle = .fullScreen
+        }
+        
+        if let root = UIApplication.shared.keyWindow?.rootViewController {
+            root.present(eventModalVC, animated: true, completion: {
+                UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
+            })
+        }
+    }
 }
+
+extension SwiftAdd2CalendarPlugin: EKEventEditViewDelegate {
+    
+    public func eventEditViewController(_ controller: EKEventEditViewController, didCompleteWith action: EKEventEditViewAction) {
+        controller.dismiss(animated: true, completion: {
+            UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+        })
+    }
+}
+
