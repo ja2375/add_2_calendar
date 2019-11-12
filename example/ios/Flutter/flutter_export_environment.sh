ac92b50d275c4acba82f497172a992b98b69c68f
Javi Hurtado <javier.hurtado@edelvives.es>
2019-11-12 18:11:06 +0100

    Update Podfile

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ja2375/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ja2375/Desktop/projs_flutter/plugins/add_2_calendar/example"
+export "FLUTTER_TARGET=/Users/ja2375/Desktop/projs_flutter/plugins/add_2_calendar/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ja2375/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
